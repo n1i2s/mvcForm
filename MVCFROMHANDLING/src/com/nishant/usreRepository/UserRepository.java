e9fd3f4a54ac35346915b3f6dc90695845cc190a
nishant chahal <nishant chahal@nishantchahal>
2019-01-15 11:26:25 +0530

    sony

---
+++ MVCFROMHANDLING/src/com/nishant/usreRepository/UserRepository.java
@@ -0,0 +1,9 @@
+package com.nishant.usreRepository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.nishant.model.User;
+
+public interface  UserRepository extends JpaRepository<User,Integer>{
+
+}
